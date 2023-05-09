64ea7ab122a779f3f3d13bf817133a6ee85198c0
GILMAR ROSSLER RAMIRES <112718649+Gilmarramires@users.noreply.github.com>
2023-05-09 19:04:53 -0300

    firebaseConfig.js

--- src/services/firebaseConfig.js
+++ src/services/firebaseConfig.js
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDCGW9By_WEM2SjAKi0_jkPRwoEk6ONMZg",
-    authDomain: "chatmeu-d3677.firebaseapp.com",
-    projectId: "chatmeu-d3677",
-    storageBucket: "chatmeu-d3677.appspot.com",
-    messagingSenderId: "599877086814",
-    appId: "1:599877086814:web:95d0c08eb5527e065acecf"
+    apiKey: --------------------
+    authDomain: -------------------
+    projectId: -------------------
+    storageBucket: -------------------
+    messagingSenderId: ----------
+    appId: -----------
 };
 
 export const app = initializeApp(firebaseConfig);
